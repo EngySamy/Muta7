e8263ea908726ec4c5c3289d3a0d0e07d8ec3efe
Engy Samy <Engy Samy>
2017-07-28 02:49:49 +0300

    finishing general info validations

--- app/src/main/java/com/muta7/muta7/CreateSpace/GeneralInfoFragment.java
+++ app/src/main/java/com/muta7/muta7/CreateSpace/GeneralInfoFragment.java
@@ -1,5 +1,6 @@
 package com.muta7.muta7.CreateSpace;
 
+import android.nfc.tech.NfcA;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
@@ -40,11 +41,26 @@ public class GeneralInfoFragment extends CreateSpaceFragmentBase {
         Insta=(TextInputEditText) rootView.findViewById(R.id.SpaceInstaValue);
         Yt=(TextInputEditText) rootView.findViewById(R.id.SpaceYtValue);
 
-        Desc.addTextChangedListener(new TextValidator(Desc) {
-            @Override public void validate(TextView textView, String text) {
-                String str=Desc.getText().toString();
-                if(str.length()>10)
-                    Desc.setError("Description should not exceed 200 character");
+        Name.setOnFocusChangeListener(new View.OnFocusChangeListener()
+        {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus)
+            {
+                if (!hasFocus) {
+                    Validations.validateSpaceName(Name,getContext());
+                }
+            }
+        });
+
+
+        Desc.setOnFocusChangeListener(new View.OnFocusChangeListener()
+        {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus)
+            {
+                if (!hasFocus) {
+                    Validations.validateDesc(Desc,getContext());
+                }
             }
         });
 
@@ -66,7 +82,7 @@ public class GeneralInfoFragment extends CreateSpaceFragmentBase {
             public void onFocusChange(View v, boolean hasFocus)
             {
                 if (!hasFocus) {
-                    Validations.validateEmail(Email);
+                    Validations.validateEmail(Email,getContext());
                 }
             }
         });
@@ -78,57 +94,23 @@ public class GeneralInfoFragment extends CreateSpaceFragmentBase {
             public void onFocusChange(View v, boolean hasFocus)
             {
                 if (!hasFocus) {
-                    Validations.validateWebsite(Website);
+                    Validations.validateWebsite(Website,getContext());
                 }
             }
         });
 
-
-        /*Fb.setOnFocusChangeListener(new View.OnFocusChangeListener()
-        {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus)
-            {
-                if (!hasFocus)
-                {
-                    String str=Fb.getText().toString();
-                    if(!TextUtils.isEmpty(str))
-                    {
-                        String regex = "((http|https)://)?(www[.])?facebook.com/.+";
-                        if(!str.matches(regex))
-                            Fb.setError( "This is not a valid facebook url" );
-                    }
-                }
-            }
-        });
-
-        Yt.setOnFocusChangeListener(new View.OnFocusChangeListener()
-        {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus)
-            {
-                if (!hasFocus)
-                {
-                    String str=Yt.getText().toString();
-                    if(!TextUtils.isEmpty(str))
-                    {
-                        String regex = "((http|https):\\/\\/|)(www\\.|)youtube\\.com\\/(channel\\/|user\\/)[a-zA-Z0-9\\-]{1,}";
-                        if(!str.matches(regex))
-                            Website.setError( "This is not a valid youtube channel url" );
-                    }
-                }
-            }
-        });*/
-
-
+        //I didn't add the social media as they don't need thing except not to exceed the max limit , we check this finally
 
         return rootView;
     }
 
     @Override
     public boolean validate(){
-        //TODO : implement the validation
-        return true;
+        return Validations.validateSpaceName(Name,getContext())&&Validations.validateDesc(Desc,getContext())
+                &&Validations.validateMobile(Mobile) && Validations.validateEmail(Email,getContext())
+                &&Validations.validateWebsite(Website,getContext())&&Validations.validateSocialMedia(Fb,getContext())
+                &&Validations.validateSocialMedia(Tw,getContext())&&Validations.validateSocialMedia(Insta,getContext())
+                &&Validations.validateSocialMedia(Yt,getContext());
     }
 
     @Override
--- app/src/main/java/com/muta7/muta7/generalResourses/Validations.java
+++ app/src/main/java/com/muta7/muta7/generalResourses/Validations.java
@@ -1,27 +1,32 @@
 package com.muta7.muta7.generalResourses;
 
+import android.content.Context;
 import android.support.design.widget.TextInputEditText;
 import android.text.TextUtils;
 import android.util.Patterns;
+
+import com.muta7.muta7.R;
 
 /**
  * Created by DeLL on 27/07/2017.
  */
 
 public final class Validations {
-    public static boolean validateEmail(TextInputEditText Email){
+    public static boolean validateEmail(TextInputEditText Email, Context context){
         String email = Email.getText().toString().trim();
-        if(TextUtils.isEmpty(email) || email.length()>256 || !Patterns.EMAIL_ADDRESS.matcher(email).matches() ){
+        int max= context.getResources().getInteger(R.integer.max_length_email);
+        if(TextUtils.isEmpty(email) || email.length()>max || !Patterns.EMAIL_ADDRESS.matcher(email).matches() ){
             Email.setError( "This email address is not valid" );
             return false;
         }
         return true;
     }
 
-    public static boolean validateWebsite(TextInputEditText Website){
+    public static boolean validateWebsite(TextInputEditText Website,Context context){
         String str=Website.getText().toString();
+        int max= context.getResources().getInteger(R.integer.max_length_website);
         if(!TextUtils.isEmpty(str)) {
-            if(str.length()>500) { //to change
+            if(str.length()>max) { //to change
                 Website.setError("This website address exceeds the max limit ");
                 return false;
             }
@@ -74,20 +79,50 @@ public final class Validations {
             return false;
         }
         String Regex="[a-zA-Z\\s']+";
-        String str=FullName.getText().toString();
-        boolean t=str.matches(Regex);
-        if(!t){
+        if(!name.matches(Regex)){
             FullName.setError( "Full name should be alphabetic" );
             return false;
         }
         return true;
     }
 
-    /*public static boolean validateLength(String text,int len){
-        if(text.length()>len){
-
+    public static boolean validateDesc(TextInputEditText Desc, Context context){
+        String str=Desc.getText().toString().trim();
+        int max= context.getResources().getInteger(R.integer.max_length_desc);
+        if (TextUtils.isEmpty(str)) {
+            Desc.setError("Description is required");
             return false;
         }
+        if (str.length() > max) {
+            Desc.setError("This description exceeds the max limit");
+            return false;
+        }
+        return true;
+    }
 
-    }*/
+    public static boolean validateSpaceName(TextInputEditText SpaceName,Context context){
+        String name = SpaceName.getText().toString().trim();
+        int max= context.getResources().getInteger(R.integer.max_length_space_name);
+        if (TextUtils.isEmpty(name)) {
+            SpaceName.setError("Space name is required");
+            return false;
+        }
+        if (name.length() > max) {
+            SpaceName.setError("This name exceeds the max limit");
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean validateSocialMedia(TextInputEditText media,Context context){
+        String str = media.getText().toString().trim();
+        int max= context.getResources().getInteger(R.integer.max_length_social_media);
+        if(!TextUtils.isEmpty(str)) {
+            if (str.length() > max) {
+                media.setError("This field exceeds the max limit ");
+                return false;
+            }
+        }
+        return true;
+    }
 }
