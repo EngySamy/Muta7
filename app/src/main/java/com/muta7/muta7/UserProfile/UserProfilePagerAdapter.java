3a5e698c6081f5198fda25a525ae2327080f1015
Engy Samy <Engy Samy>
2017-08-03 21:22:52 +0300

    Creates user profile classes

---
+++ app/src/main/java/com/muta7/muta7/UserProfile/UserProfilePagerAdapter.java
@@ -0,0 +1,32 @@
+package com.muta7.muta7.UserProfile;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by DeLL on 03/08/2017.
+ */
+
+public class UserProfilePagerAdapter extends FragmentPagerAdapter {
+    public UserProfilePagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment obj;
+        switch (position){
+            case 0:obj=new InfoFragment();
+                break;
+            default:obj=new InfoFragment();
+                break;
+        }
+        return obj;
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+}
